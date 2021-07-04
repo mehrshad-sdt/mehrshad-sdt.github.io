03baabba8ffd46af7d971f7980289c1d09c849fe
mehrshad_sdt <63510622+mehrshad-sdtn@users.noreply.github.com>
2021-07-04 23:17:09 +0430

    Update calc.js

--- calc.js
+++ calc.js
@@ -3,5 +3,5 @@ var button = document.querySelector('#btn');
 button.addEventListener('click', () => {
     var number = document.querySelector('#num').value;
     var percent = document.querySelector('#perc').value;
-    alert(parseFloat( number) * (parseFloat(percent) / 100));
+    console.log(parseFloat( number) * (parseFloat(percent) / 100));
 })
